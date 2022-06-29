4efc97b4c1d2cdef6248ab92dc69ee3880825d75
Diego Gonzalez <degonzalez6@gmail.com>
2022-06-29 12:02:28 -0300

    Se sube ejercicio 2 sobre cálculo de millas

---
+++ src/main/java/CalculoMillas.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CalculoMillas {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese un valor en millas:");
+        int numero = sc.nextInt();
+        System.out.println("El valor ingresado en Millas corresponde a: " + numero * 1.60934 + "Kms");
+        while (numero>0){
+
+            numero = sc.nextInt();
+            System.out.println("El valor ingresado en Millas corresponde a: " + numero * 1.60934 + "Kms");
+        }
+        System.out.println("Ha salido del sistema");
+
+    }
+
+}
